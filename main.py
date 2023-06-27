fcd1aa97e5a832a9b18e00cbcbbe81489d3414e3
sanziolopes <108752809+sanziolopes@users.noreply.github.com>
2023-06-27 20:56:08 -0300

    reik of lake

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2.driver import Driver
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
@@ -7,12 +8,15 @@ ENVIRONMENT = 'SERVER'
 
 if ENVIRONMENT == 'SERVER':
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    print(chrome_options)
-    driver = webdriver.Chrome(chrome_options,os.environ.get("CHROMEDRIVER_PATH"),True)
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    try:
+        webdriver.Chrome(chrome_options=chrome_options,service = os.environ.get("CHROMEDRIVER_PATH"))
+    except:
+        driver = webdriver.Chrome(chrome_options,os.environ.get("CHROMEDRIVER_PATH"),True)
+    
 
 if ENVIRONMENT == 'LOCAL':
     chrome_options = webdriver.ChromeOptions()
