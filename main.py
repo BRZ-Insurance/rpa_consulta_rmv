38785d0a1f36409c8310b70019bf18d5315b494d
sanziolopes <108752809+sanziolopes@users.noreply.github.com>
2023-06-27 18:27:08 -0300

    alterações plymouth

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
 import os
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+ENVIRONMENT = 'SERVER'
 
-# Now you can start using Selenium
+if ENVIRONMENT == 'SERVER':
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    print(chrome_options)
+    driver = webdriver.Chrome(service=os.environ.get("CHROMEDRIVER_PATH"),options=chrome_options,keep_alive=True)
 
-driver.get("https://www.google.com")
-print(driver.page_source)
+if ENVIRONMENT == 'LOCAL':
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://agentweb2.plymouthrock.com/aiui/login")
+print(driver.current_url)
+driver.find_element(By.CSS_SELECTOR,'[id="j_username"]').send_keys('slopes_brz')
+driver.find_element(By.CSS_SELECTOR,'[id="j_password"]').send_keys('As12!@RPA')
+driver.find_element(By.CSS_SELECTOR,'[id="login"]').click()
+sleep(3)
+print(driver.current_url)
+
