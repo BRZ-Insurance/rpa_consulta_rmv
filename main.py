ab77b8c913079b307a66486e09fd7e5fcefa6897
sanziolopes <108752809+sanziolopes@users.noreply.github.com>
2023-07-08 01:09:06 -0300

    tirei o headless

--- main.py
+++ main.py
@@ -52,8 +52,8 @@ def init():
     match ENVIRONMENT:
         case 'SERVER_CHROME':
             chrome_options = webdriver.ChromeOptions()
-            chrome_options.add_argument("--headless=new")
-            chrome_options.add_argument("--disable-dev-shm-usage")
+            # chrome_options.add_argument("--headless=new")
+            # chrome_options.add_argument("--disable-dev-shm-usage")
             chrome_options.add_argument("--no-sandbox")
             chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
             chrome_service =  service.ChromiumService(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
